f223393bd51f798e103242276efb10891b1d26ad
Silenrate <daniwalteros596@gmail.com>
2021-02-11 13:42:27 -0500

    Creating Login Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import {Login} from './components/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    localStorage.setItem("Username", "daniel@gmail.com");
+    localStorage.setItem("Password", "12345");
+
+    const[isLoggedInState,setIsLoggedInState] = useState(localStorage.getItem("isLoggedIn"));
+
+    const handleSuccessfullyLogin = (e) => {
+        console.log("Logueado");
+        setIsLoggedInState(true);
+        localStorage.setItem("isLoggedIn", true);
+    }
+
+    const handleFailedLogin = (e) => {
+        alert("Usuario o Clave Incorrectos");
+        setIsLoggedInState(false);
+        localStorage.setItem("isLoggedIn", false);
+    }
+
+    return (
+        <div className="App">
+            <Login successful={handleSuccessfullyLogin} failed={handleFailedLogin}/>
+        </div>
+    );
 }
 
 export default App;
