23c6b5bc5f7d12223bf7158df3d7a34647611d56
Silenrate <daniwalteros596@gmail.com>
2021-02-11 15:33:18 -0500

    NavBar Component

--- src/App.js
+++ src/App.js
@@ -22,34 +22,39 @@ const App = () => {
         localStorage.setItem("isLoggedIn", false);
     }
 
+    const handleLogout = () => {
+        setIsLoggedInState(false);
+        localStorage.setItem("isLoggedIn", false);
+    }
+
     const items = [{
         "description": "Do The NavBar",
         "responsible": {
-            "name": "Santiago Carrillo",
-            "email": "sancarbar@gmail.com"
+            "name": "Daniel Walteros",
+            "email": "daniel@gmail.com"
         },
         "status": "In Progress",
         "dueDate": 156464645646
         },{
         "description": "Eat Lunch",
         "responsible": {
-            "name": "Santiago Carrillo",
-            "email": "sancarbar@gmail.com"
+            "name": "Daniel Walteros",
+            "email": "daniel@gmail.com"
         },
         "status": "Ready",
         "dueDate": 156464645646
         },{
         "description": "Go to the doctor",
         "responsible": {
-            "name": "Santiago Carrillo",
-            "email": "sancarbar@gmail.com"
+            "name": "Daniel Walteros",
+            "email": "daniel@gmail.com"
         },
         "status": "Completed",
         "dueDate": 156464645646}
     ];
 
     const LoginView = () => (<Login successful={handleSuccessfullyLogin} failed={handleFailedLogin}/>);
-    const MainView = () => (<Main items={items}/>);
+    const MainView = () => (<Main items={items} logout={handleLogout} email={localStorage.getItem("Username")}/>);
 
     return (
         <div className="App">
--- src/components/Main.js
+++ src/components/Main.js
@@ -6,11 +6,13 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import {Task} from './Task';
+import {NavBar} from './NavBar';
 
 export const Main = (props) => {
 
     return(
         <div>
+            <NavBar logout={props.logout} email={props.email}/>
             {props.items.map((item,i) => {
                 return (<Task key={i}
                     description={item.description}
